903201a4d57797fd68108e646686b5f9cb400b2d
tripathiankit28 <tripathiankit2809@gmail.com>
2022-02-17 21:40:53 +0530

    Initial commit

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `<p>Footer is made using {{name}}</p>
+  <p> button using event binding </p> 
+  <button (click)=onclick()>Click Me!</button>`,
+  styleUrls: ['./footer.component.css']
+  
+})
+export class FooterComponent implements OnInit {
+  public name ="Interpolation";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onclick(){
+    console.log("Welcome to Credex");
+  }
+
+}
