168e29a4f2b34036946e1c98a5fa02e4d1f2d4be
tripathiankit28 <tripathiankit2809@gmail.com>
2022-02-18 21:02:31 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
